d613dd4515616ab57926d28bb2827ffe5b2457ca
alexey <alxtrusov@gmail.com>
2021-05-08 09:41:43 +0400

    more comments

--- src/components/calculator/CalculatorComponent.js
+++ src/components/calculator/CalculatorComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 // файлы с калькулятором писать тут
+//...
 
 class CalculatorComponent extends React.Component {
     render() {
         // разметку писать тут
+        //...
         return (
             <div className="calculator">
                 Калькулятор!!!
